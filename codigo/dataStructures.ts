64674b38af8a4a15de9ab8702f9ad0fbefb03b76
Edgar Chaver <edieveg316@gmail.com>
2024-06-18 08:31:06 -0600

    Update dataStructures.ts

--- codigo/dataStructures.ts
+++ codigo/dataStructures.ts
@@ -778,12 +778,3 @@ console.log(
 		[4, 2, 1],
 	])
 );
-
-type InferAsTuple = [unknown, ...unknown[]];
-
-const options = [
-	{ label: "View", value: "VIEW" },
-	{ label: "Full access", value: "FULL_ACCESS" },
-] satisfies InferAsTuple;
-
-console.log(options[2]);
