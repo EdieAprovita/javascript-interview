829f7636c8959f8879e1b1af116341f7c670c892
Edgar Chaver <edieveg316@gmail.com>
2024-06-25 19:54:57 -0600

    Update dataStructures.ts

--- codigo/dataStructures.ts
+++ codigo/dataStructures.ts
@@ -905,3 +905,33 @@ function candy(ratings: number[]): number {
 }
 
 console.log(candy([1, 0, 2]));
+
+function trap(height: number[]): number {
+	let left = 0;
+	let right = height.length - 1;
+	let leftMax = 0;
+	let rightMax = 0;
+	let result = 0;
+
+	while (left < right) {
+		if (height[left] < height[right]) {
+			if (height[left] >= leftMax) {
+				leftMax = height[left];
+			} else {
+				result += leftMax - height[left];
+			}
+			left++;
+		} else {
+			if (height[right] >= rightMax) {
+				rightMax = height[right];
+			} else {
+				result += rightMax - height[right];
+			}
+			right--;
+		}
+	}
+
+	return result;
+}
+
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
