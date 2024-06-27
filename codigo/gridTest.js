bbe33d62222f669db98e4d8d20e48fbf1dec8b7e
Edgar Chaver <edieveg316@gmail.com>
2024-06-26 18:45:13 -0600

    Create gridTest.js

---
+++ codigo/gridTest.js
@@ -0,0 +1,15 @@
+console.log(number);
+
+let number = 10;
+
+function sing() {
+	// declaration
+	console.log("la la la");
+}
+let dance = function () {
+	// expression
+	console.log("cha cha cha");
+};
+
+sing();
+dance();
